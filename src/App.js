468ca4ba56374d8834f396bf5a1bd10f0710b083
Sidharth <sidharthpunalur@gmail.com>
2024-02-15 08:52:35 +0530

    Fevicon bug fixed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import './App.css';
-import CardSection1 from './Components/CardSection1';
-import CardSection2 from './Components/CardSection2';
-import Footer from './Components/Footer';
-import NavBar from './Components/NavBar'
-import P1Content from './Components/P1Content';
+
 
 function App() {
   return (
   <div>
+
     </div>
   );
 }
