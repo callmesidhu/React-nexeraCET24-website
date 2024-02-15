318396b8511c54fb33e6df7103e06086610ada03
Sidharth <sidharthpunalur@gmail.com>
2024-02-15 10:48:46 +0530

    Responsive background added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
-
+import React from 'react';
+import fullBG from './Images/fullBG.png';
 
 function App() {
   return (
   <div>
-
+    
+  
     </div>
   );
 }
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -4,8 +4,7 @@ import "./Header.css";
 function NavBar() {
   return (
     <div>
-        
-
+    
 
     </div>
   )
