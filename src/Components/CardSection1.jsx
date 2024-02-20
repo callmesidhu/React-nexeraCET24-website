4ebf747efcb47eb1faa96fbd23682d40b6ad52ab
Sidharth <sidharthpunalur@gmail.com>
2024-02-20 23:43:18 +0530

    all Work completed and temporary animation added

--- src/Components/CardSection1.jsx
+++ src/Components/CardSection1.jsx
@@ -2,15 +2,29 @@ import React from 'react';
 import './CardSection.css';
 import eventCard from '../Cards/eventCard.jpeg';
 import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
+function xScrollLeft(){
+  var left = document.querySelector(".card-main-row");
+  left.scrollBy(500,0)
+}
+function xScrollRight(){
+  var right = document.querySelector(".card-main-row");
+  right.scrollBy(-500,0)
+  
+}
 
 function CardSection1() {
+
   return (
     <div>
       <div className="flex w-full" id="events">
         <h1 className="card-title float-left w-full">Pre Events</h1>
         <div className="float-right flex pt-2 lg:mx-48 md:mx-24 sm:mx-24 lg:px-24" >
+          <button onClick={xScrollRight}>
           <MdChevronLeft id="slider1" className="cursor-pointer" size={50} />
+          </button>
+          <button onClick={xScrollLeft}>
           <MdChevronRight id="slider2" className="cursor-pointer" size={50} />
+          </button>
         </div>
       </div>
       <div className="card-scroll-container">
